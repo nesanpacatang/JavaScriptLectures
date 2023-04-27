94f9af0f70ab4d6742d7d69ba501f2352d1644ef
unknown <nesanpacatang@gmail.com>
2023-04-27 15:36:51 +0800

    initial commit

---
+++ 01-variable-datatypes/02comment-shortcuts/script.js
@@ -0,0 +1,9 @@
+// Single line of code comment
+
+// The shortcut key is ctrl + /
+
+// multiline comment shortcut key : Shift + Alt A
+
+/* console.log(100);
+console.log ("Hello World");
+console.log (20,"Hello", true) */
