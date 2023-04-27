62fcf1848fa3016c9d279dd5e47e3c94ef09964b
unknown <nesanpacatang@gmail.com>
2023-04-27 15:09:47 +0800

    initial commit

---
+++ 01-variable-datatypes/01console/script.js
@@ -0,0 +1,29 @@
+// alert("Hello from JS file");
+
+// log a number
+console.log(100);
+
+// log string
+console.log("Hello World");
+
+// log multiple values
+console.log(20, "Hello", true);
+
+// log a vaialble
+
+const x = 100;
+console.log(x);
+
+// console error & warning
+console.error("Alert");
+console.warn("Warning");
+
+// log object as a table
+console.table({ name: "Nesan", email: "nesanpacatang@gmail.com" });
+
+// group console command
+console.group("simple");
+console.log(x);
+console.error("Alert");
+console.warn("Warning");
+console.group();
