1ebee397c4229f34f9bda6020c898d5e982122e4
unknown <nesanpacatang@gmail.com>
2023-05-03 19:34:10 +0800

    file update

---
+++ 05-iteration-array-methods/04challenge-fizzbuzz/script.js
@@ -0,0 +1,41 @@
+// for (let i = 0; i <= 100; i++) {
+//   let j = i % 3;
+//   let k = i % 5;
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// # FizzBuzz Challenge
+
+// **Instructions:**
+
+// - Print/log the numbers from 1 to 100
+// - For **multiples of three** print "Fizz" instead of the number
+// - For **multiples of five** print "Buzz"
+// - For numbers which are **multiples of both three and five** print "FizzBuzz".
+
+// **Hints:**
+
+// Write a loop and output/log something for each iteration. You also know how to check for a condition with "if/else/else if". You also know how to get a remainder of a number using the modulus operator (%). This is all you need to know to complete this challenge. Good luck!
