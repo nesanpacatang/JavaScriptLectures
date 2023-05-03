ea35b16e48f15ae2d6e6b83cfaaf61044dc79130
unknown <nesanpacatang@gmail.com>
2023-05-03 08:16:24 +0800

    file update

---
+++ 03-functions-scope/09challenge/script.js
@@ -0,0 +1,27 @@
+// Challenge 1
+
+// function getCelsius(f) {
+//   return ((f - 32) * 5) / 9;
+// }
+// console.log(getCelsius(32));
+
+// Bunos
+
+// const getCelcius = (f) => ((f - 32) * 5) / 9;
+
+// console.log(getCelcius(32));
+
+// putting into a string
+// function getCelsius(f) {
+//   return ((f - 32) * 5) / 9;
+// }
+
+// console.log(`The temperature is ${getCelsius(32)} \xB0C`);
+
+// Challenge 2
+const arr = [1, 2, 3, 4, 5];
+function minMax() {
+  return Math.min(...arr), Math.max(...arr);
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
